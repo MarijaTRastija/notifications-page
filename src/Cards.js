f42aaa1cfe16b890f73eda32e42a0f6bce93d7d0
MarijaTRastija <101635462+MarijaTRastija@users.noreply.github.com>
2023-03-20 17:08:59 +0100

    Added an img, dot, centered the img

--- src/Cards.js
+++ src/Cards.js
@@ -7,6 +7,7 @@ import hasanuddin from "./images/avatar-rizky-hasanuddin.webp";
 import smith from "./images/avatar-kimberly-smith.webp";
 import peterson from "./images/avatar-nathan-peterson.webp";
 import kim from "./images/avatar-anna-kim.webp";
+import you from "./images/image-chess.webp";
 
 // const cards = [
 //   {
@@ -43,131 +44,115 @@ export default function Cards(props) {
           <p>{card.date}</p>
         </li>
       ))} */}
-      <table>
-        <li className={readClassName}>
-          <tr>
-            <td>
-              <img src={webber} alt="Mark Webber" />
-            </td>
-            <td>
-              <div className="redDot">
-                <a href="/" className="nameLink">
-                  Mark Webber
-                </a>{" "}
-                reacted to your recent post today{" "}
-                <a href="/" className="postLink">
-                  My first tournament today!
-                </a>
-              </div>
-              <p>1m ago</p>
-            </td>
-          </tr>
-        </li>
+      {/* <table> */}
+      <li className={readClassName}>
+        <div className="rowLi">
+          <img src={webber} alt="Mark Webber" />
 
-        <li className={readClassName}>
-          <tr>
-            <td>
-              <img src={gray} alt="Angela Gray" />
-            </td>
-            <td>
-              <a href="/" className="nameLink">
-                Angela Gray
-              </a>{" "}
-              followed you
-              <p>5m ago</p>
-            </td>
-          </tr>
-        </li>
-
-        <li className={readClassName}>
-          <tr>
-            <td>
-              <img src={thompson} alt="Jacob Thompson" />
-            </td>
-            <td>
-              <a href="/" className="nameLink">
-                Jacob Thompson
-              </a>{" "}
-              has joined your group{" "}
-              <a href="/" className="chessLink">
-                Chess Club
-              </a>
-              <p>1 day ago</p>
-            </td>
-          </tr>
-        </li>
-
-        <li className="notification">
-          <tr>
-            <td>
-              <img src={hasanuddin} alt="Rizky Hasanuddin" />
-            </td>
-            <td>
-              <a href="/" className="nameLink">
-                Rizky Hasanuddin
-              </a>{" "}
-              sent you a private message
-              <p>5 days ago</p>
-              <br />
-              <a href="/" className="message">
-                Hello, thanks for setting up the Chess Club. I've been a member
-                for a few weeks now and I'm already having lots of fun and
-                improving my game.
-              </a>
-            </td>
-          </tr>
-        </li>
-
-        <li className="notification">
-          <tr>
-            <td>
-              <img src={smith} alt="Kimberly Smith" />
-            </td>
-            <td>
-              <a href="/" className="nameLink">
-                Kimberly Smith
-              </a>{" "}
-              commented on your picture
-              <img src="" alt="" />
-              <p>1 week ago</p>
-            </td>
-          </tr>
-        </li>
-        <li className="notification">
-          <tr>
-            <td>
-              <img src={peterson} alt="Nathan Peterson" />
-            </td>
-            <td>
-              <a href="/" className="nameLink">
-                Nathan Peterson
-              </a>{" "}
-              reacted to your recent post today{" "}
-              <a href="/" className="postLink">
-                5 end-game strategies to increase your win rate
-              </a>
-              <p>2 weeks ago</p>
-            </td>
-          </tr>
-        </li>
-        <li className="notification">
-          <tr>
-            <td>
-              <img src={kim} alt="Anna Kim" />
-            </td>
-            <td>
-              <a href="/" className="nameLink">
-                Anna Kim
-              </a>{" "}
-              left the group{" "}
-              <a href="/" className="chessLink">
-                Chess club
-              </a>
-              <p>2 weeks ago</p>
-            </td>
-          </tr>
-        </li>
-      </table>
+          <div className="notificationText">
+            <a href="/" className="nameLink">
+              Mark Webber
+            </a>{" "}
+            reacted to your recent post today{" "}
+            <a href="/" className="postLink">
+              My first tournament today!
+            </a>
+            <div className="redDot"></div>
+            <p>1m ago</p>
+          </div>
+        </div>
+      </li>{" "}
+      <li className={readClassName}>
+        <div className="rowLi">
+          <img src={gray} alt="Angela Gray" />
+          <div className="notificationText">
+            <a href="/" className="nameLink">
+              Angela Gray
+            </a>{" "}
+            followed you
+            <div className="redDot"></div>
+            <p>5m ago</p>
+          </div>
+        </div>
+      </li>
+      <li className={readClassName}>
+        <div className="rowLi">
+          <img src={thompson} alt="Jacob Thompson" />
+          <div className="notificationText">
+            <a href="/" className="nameLink">
+              Jacob Thompson
+            </a>{" "}
+            has joined your group{" "}
+            <a href="/" className="chessLink">
+              Chess Club
+            </a>
+            <div className="redDot"></div>
+            <p>1 day ago</p>
+          </div>
+        </div>
+      </li>
+      <li className="notification">
+        <div className="rowLi">
+          <img src={hasanuddin} alt="Rizky Hasanuddin" />
+          <div className="notificationText">
+            <a href="/" className="nameLink">
+              Rizky Hasanuddin
+            </a>{" "}
+            sent you a private message
+            <p>5 days ago</p>
+            <br />
+            <a href="/" className="message">
+              Hello, thanks for setting up the Chess Club. I've been a member
+              for a few weeks now and I'm already having lots of fun and
+              improving my game.
+            </a>
+          </div>
+        </div>
+      </li>
+      <li className="notification">
+        <div className="rowLi">
+          <img src={smith} alt="Kimberly Smith" />
+          <div className="notificationText">
+            <a href="/" className="nameLink">
+              Kimberly Smith
+            </a>{" "}
+            commented on your picture
+            <img src="" alt="" />
+            <p>1 week ago</p>
+          </div>{" "}
+          <img src={you} alt="" className="yourImage" />{" "}
+        </div>
+      </li>
+      <li className="notification">
+        <div className="rowLi">
+          <img src={peterson} alt="Nathan Peterson" />
+          <div className="notificationText">
+            <a href="/" className="nameLink">
+              Nathan Peterson
+            </a>{" "}
+            reacted to your recent post today{" "}
+            <a href="/" className="postLink">
+              5 end-game strategies to increase your win rate
+            </a>
+            <p>2 weeks ago</p>
+          </div>
+        </div>
+      </li>
+      <li className="notification">
+        <div className="rowLi">
+          <img src={kim} alt="Anna Kim" />
+          <div className="notificationText">
+            <a href="/" className="nameLink">
+              Anna Kim
+            </a>{" "}
+            left the group{" "}
+            <a href="/" className="chessLink">
+              Chess club
+            </a>
+            <p>2 weeks ago</p>
+          </div>
+        </div>
+      </li>
     </ul>
   );
 }
